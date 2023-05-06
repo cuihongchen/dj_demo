62483202df14609b3d31f3d3c30259ca3cc380de
cuihc <cuihc@feidai.com>
2023-05-06 15:35:33 +0800

    修改URL显示标签名称

--- test_app/admin.py
+++ test_app/admin.py
@@ -4,3 +4,5 @@ from django.contrib import admin
 from test_app.models import Topic,Entry
 admin.site.register(Topic)
 admin.site.register(Entry)
+admin.site.site_header('学习笔记')
+admin.site.site_title('学习笔记')
